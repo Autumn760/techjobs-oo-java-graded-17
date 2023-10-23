b1849bc8777174308fddb99b0f9704ea6c09b1ae
Stephanie Ontiveros <autumn760@gmail.com>
2023-10-23 11:16:12 -0500

    Task 2, complete.

--- src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
+++ src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
@@ -39,6 +39,18 @@ public class CoreCompetency {
     }
 
     // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
-    //  ONLY a getter for the 'id' field.
+    //  ONLY a getter for the 'id' field. (DOne)
 
+
+    public String getValue() {
+        return value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
